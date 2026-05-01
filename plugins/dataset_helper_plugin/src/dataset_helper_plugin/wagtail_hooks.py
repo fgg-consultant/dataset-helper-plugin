a7561d4e53ca94e71e1b49da821956ac62471a01
FGG Consultant <florent.gravin@gmail.com>
2026-05-01 10:30:59 +0200

    Move plugin to geomanager and use star as icon

--- plugins/dataset_helper_plugin/src/dataset_helper_plugin/wagtail_hooks.py
+++ plugins/dataset_helper_plugin/src/dataset_helper_plugin/wagtail_hooks.py
@@ -8,11 +8,11 @@ from . import urls as plugin_urls
 @hooks.register('register_admin_urls')
 def register_dataset_helper_url():
     return [
-        path('dataset_helper/', include((plugin_urls, 'dataset_helper_plugin'))),
+        path('geomanager/dataset_helper/', include((plugin_urls, 'dataset_helper_plugin'))),
     ]
 
 
-@hooks.register('register_admin_menu_item')
-def register_admin_menu_item():
+@hooks.register('register_geo_manager_menu_item')
+def register_geo_manager_dataset_helper_item():
     url = reverse('dataset_helper_plugin:index')
-    return MenuItem('Dataset helper', url, icon_name='globe', order=300)
+    return MenuItem('Dataset helper', url, icon_name='pick', order=1.5)
